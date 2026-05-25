6cde1c51102ff8d3d12750551b827455a580562c
Adeel Farooq <adeelkhattak88088@gmail.com>
2026-05-25 17:23:26 +0500

    App.js Optimized

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
     ScrollSmoother.create({
       wrapper: '#smooth-wrapper',
       content: '#smooth-content',
-      smooth: 1.5, // Same as first code (best for performance)
+      smooth: 2, // Same as first code (best for performance)
       effects: true,
       normalizeScroll: true, // Touch devices pe scroll bugs fix karta hai
     });
