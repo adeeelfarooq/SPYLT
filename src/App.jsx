3a9de8dad2e9a092ba3c559b3c52f86c9f693657
Adeel Farooq <adeelkhattak88088@gmail.com>
2026-05-25 17:17:41 +0500

    App.js Optimized

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-
-import NavBar from './components/NavBar'
-import Herosection from './sections/Herosection'
-import { ScrollSmoother, ScrollToPlugin , ScrollTrigger } from 'gsap/all';
+import React, { useRef } from 'react';
+import NavBar from './components/NavBar';
+import Herosection from './sections/Herosection';
+import { ScrollSmoother, ScrollToPlugin, ScrollTrigger } from 'gsap/all';
 import gsap from 'gsap';
 import Messagesection from './sections/Messagesection';
 import FlavourSection from './sections/FlavourSection'; 
@@ -11,40 +11,55 @@ import Benefitsection from './sections/Benefitsection';
 import Testimonialsection from './sections/Testimonialsection';
 import Footersection from './sections/Footersection';
 
-gsap.registerPlugin(ScrollTrigger , ScrollToPlugin ,  ScrollSmoother);
+// Plugins Register karna lazmi hai
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin, ScrollSmoother);
 
 const App = () => {
+  const mainRef = useRef(null); // Pure app ka ref
 
-  useGSAP(()=>{
-  ScrollSmoother.create({
-    smooth: 2,
-    effects: true,
-  } )
-})  
+  useGSAP(() => {
+    // 1. ScrollSmoother Create karna
+    ScrollSmoother.create({
+      wrapper: '#smooth-wrapper',
+      content: '#smooth-content',
+      smooth: 1.5, // Same as first code (best for performance)
+      effects: true,
+      normalizeScroll: true, // Touch devices pe scroll bugs fix karta hai
+    });
+
+    // 2. SAB SE BARA FIX (Sections Merging Issue)
+    // Jab images aur heavy elements load ho jayen, toh GSAP ko refresh karo
+    const timeout = setTimeout(() => {
+      ScrollTrigger.refresh();
+    }, 1000); // 1 second ka delay taa ke sab kuch render ho jaye
+
+    // Resize event pe bhi refresh karo (agar user screen choti bari kare)
+     window.addEventListener('resize', () => ScrollTrigger.refresh());
+   return () => {
+         clearTimeout(timeout);
+         window.removeEventListener('resize', () => ScrollTrigger.refresh());
+       };
+     }, { scope: mainRef }); 
+
   return (
-    
+    <main ref={mainRef}>
+      {/* NavBar ko fixed rakhne k liye wrapper se bahar rakha hai */}
+      <NavBar />
       
-        <main>
-          <NavBar />
-          <div id="smooth-wrapper">   
-      <div id="smooth-content">
+      {/* ScrollSmoother in IDs ko dhoondta hai */}
+      <div id="smooth-wrapper">   
+        <div id="smooth-content">
           <Herosection />
           <Messagesection />
           <FlavourSection />
-          <Nutritionsection/>
-          <div>
-            <Benefitsection/>
-          <Testimonialsection/>
-          </div>
-          <Footersection/>
-          
-          </div>
-          </div>
-        </main>
-      
-    
-  
-  )
+          <Nutritionsection />
+          <Benefitsection />
+            <Testimonialsection />
+          <Footersection />
+        </div>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
