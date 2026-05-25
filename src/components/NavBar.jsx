61edecbcd41327784abfb2419e67370417e7133d
Adeel Farooq <adeelkhattak88088@gmail.com>
2026-05-25 17:32:49 +0500

    Navbar updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,9 +3,12 @@
 const NavBar = () => {
     return (
 
-        <nav className='fixed top-0 left-0 z-50 md:p-5 p-3'>
-            <img src="images/nav-logo.svg" alt="Navbar-Logo" className="md:w-24 w-20 md:scale-80" />
-        </nav>
+        <><a href="#" className="fixed z-20000 scale-80 inline-flex items-center justify-center bg-white/70 text-dark-brown font-extrabold right-[1%] top-[3%]  uppercase tracking-wide py-4 px-8 rounded-full transition-all duration-300 ease-in-out hover:bg-light-brown   ">
+            FIND IN STORES
+        </a><nav className='fixed top-0 left-0 z-50 md:p-5 p-3'>
+
+                <img src="images/nav-logo.svg" alt="Navbar-Logo" className="md:w-24 w-20 md:scale-80" />
+            </nav></>
 
     )
 }
