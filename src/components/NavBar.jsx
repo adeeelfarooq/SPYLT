8f0f931e1eb6125d367e1af66361aca10a14b5fe
Adeel Farooq <adeelkhattak88088@gmail.com>
2026-05-25 17:44:49 +0500

    Navbar updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@
 const NavBar = () => {
     return (
 
-        <><a href="#" className="fixed z-20000 scale-80 inline-flex items-center justify-center bg-white/70 text-dark-brown font-extrabold right-[1%] top-[3%]  uppercase tracking-wide py-4 px-8 rounded-full transition-all duration-300 ease-in-out hover:bg-light-brown   ">
+        <><a href="#" className="fixed z-20000 scale-80 inline-flex items-center justify-center bg-[#fef3f0]  text-dark-brown font-extrabold right-[1%] top-[3%]  uppercase tracking-wide py-4 px-8 rounded-full transition-all duration-300 ease-in-out hover:bg-light-brown   ">
             FIND IN STORES
         </a><nav className='fixed top-0 left-0 z-50 md:p-5 p-3'>
 
